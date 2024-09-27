c6c80ed0ab89e11f67cdb9095366a6b4c14147e2
ayodeji <ayodejiakintobi1@gmail.com>
2024-09-27 20:06:07 +0100

    UPDATE: FACTORY CONTRACT

--- src/Factory.sol
+++ src/Factory.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.0.13;
+
+contract Factory{
+
+    mapping(address => address) public tokenToExchange;
+    mapping(address => address) public exchnageToToken;
+    mapping(uint256 => address) public idToToken;
+    Exchange[] public exchangeArray;
+
+    event ExchangeCreated(address indexed tokenAddress, address indexed exchnageToToken);
+
+    
+}
