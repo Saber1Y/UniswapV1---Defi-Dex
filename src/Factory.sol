e81c5d43dedf09a65e2434a651b2ae904590a444
ayodeji <ayodejiakintobi1@gmail.com>
2024-09-28 15:16:34 +0100

    UPDATE: FACOTRY CONTRACT FUNCTION

--- src/Factory.sol
+++ src/Factory.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.0.13;
 
-contract Factory{
+contract Factory {
+    error NotTokenAddress();
+    error ExchangeAlreadyExists();
 
     mapping(address => address) public tokenToExchange;
     mapping(address => address) public exchnageToToken;
@@ -11,5 +12,20 @@ contract Factory{
 
     event ExchangeCreated(address indexed tokenAddress, address indexed exchnageToToken);
 
-    
+    function createNewExchange(address _tokenToExchnage) public returns (address) {
+        if (_tokenAddress == address[0]) {
+            revert NotTokenAddress();
+        }
+
+        if (tokenToExchange[_tokenAddress] != address[0]) {
+            revert ExchangeAlreadyExists();
+        }
+
+        Exchange exchange = new Exchange(_tokenAddress); //created new instance of contract Exchnage to pass argument
+        exchangeArray.push(exchnage); //adding the new contract to the exchnage array
+        tokenToExchange[_tokenAddress] = address(exchange);
+
+         emit ExchangeCreated(_tokenAddress, address(exchange));
+         return address(exchange);
+    }
 }
