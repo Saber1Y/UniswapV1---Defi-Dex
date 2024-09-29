6d86fbf1a342c72a40d0f6bc0ec9e305c45dae68
ayodeji <ayodejiakintobi1@gmail.com>
2024-09-29 12:27:11 +0100

    UPDATE: EXCHANGE

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import './ERC20.sol';
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+
+contract Exchange is ERC20, ReentrancyGuard {
+
+}
