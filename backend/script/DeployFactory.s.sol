e47abe590a59b1e55863c5ce8e2a1f43af9becf3
ayodeji <ayodejiakintobi1@gmail.com>
2024-10-08 17:48:43 +0100

    UPDATE: CA

---
+++ backend/script/DeployFactory.s.sol
@@ -0,0 +1,22 @@
+// script/DeployFactory.s.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Factory } from "../src/Factory.sol"; // Adjust the path as necessary
+
+// Import necessary libraries
+import {Script, console} from "forge-std/Script.sol";
+
+contract DeployFactory is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the Factory contract
+        Factory factory = new Factory();
+
+        // Log the deployed address
+        console.log("Factory deployed to:", address(factory));
+
+        vm.stopBroadcast();
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { WagmiProvider, useAccount } from "wagmi";
-import {  RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "./Config"; // Import the config you just created
 import "@rainbow-me/rainbowkit/styles.css";
 import AddLiquidity from "./AddLiquidity";
 import Navbar from "./constants/NavBar";
+import factoryContract from "./Data/Factory.json";
+
+const abi = factoryContract.abi;
+const contractAddress = "0x90193C961A926261B756D1E5bb255e67ff9498A1";
 
 function App() {
   const queryClient = new QueryClient();
